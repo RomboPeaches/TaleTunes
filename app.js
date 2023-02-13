fd20e27602af1c3b21be85517e18f1be400ad070
Robin <robinbenjaminvanek@gmail.com>
2023-02-13 12:46:58 +0100

    frontend: random tint for picker elem, resizing player, overflow hidden, ...

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // indicate running, volume, what else?
 // shortcuts?!
-// drag and drop url urls !!!
-// save via cookie or file (urls, set size, discription?
+// drag and drop url defaultUrls !!!
+// save via cookie or file (defaultUrls, set size, discription?
 
 // OR SELECT EFFECT i.e. FADE IN FAST and then click the iframe proxy elem
 // and vice versa: selct multiple iframes and choose effect BOTH
@@ -21,29 +21,104 @@
 
 // load players fades ... after each other?! -> sap use
 
-// clipboard urls!?! i.e. coolers 
+// clipboard defaultUrls!?! i.e. coolers 
 
 // push players into array to access functionalities
 
-// when del player leave index empty, add by pushing new index, ignore empties when save  
+// when del player leave id empty, add by pushing new id, ignore empties when save  
 
 // tooltip for describtion and way to add descriptions 
 
 // fain + play!
 
+// toggle selection onlcick
+
 //-----------------------------------------------------------------------------
 
+
+// 
 class Tune {
 
-    constructor(player, url, domId) {
+    constructor(url, id) {
 
-        this.player = player;
+        // create a container for player positioning
+        let playerContainer = document.createElement('div');
+        playerContainer.id = 'playerContainer' + String(id);
+        playerContainer.classList.add('playerContainer');
+        document.getElementById('playerContainerSection').appendChild(playerContainer);
+
+        // create picker div to tint if selected and fade in effect
+        let picker = document.createElement('div');
+        picker.id = 'picker' + id;
+        picker.classList.add('picker');
+        document.getElementById('playerContainer' + String(id)).appendChild(picker);
+
+        picker.style.backgroundColor = this.randColor();
+
+        // add onclkick functions to pickers
+        picker.onclick = function(event) {
+
+            // toggle selection (temp)
+            if (infoBar.style.opacity == 1.0) {
+                infoBar.style.opacity = 0.0;
+                picker.style.opacity = 0.3;
+            } else {
+                infoBar.style.opacity = 1.0;
+                picker.style.opacity = 0.9;
+            }
+        }
+
+        // create a temp div to be replaced by the video-iframe
+        let tempDivElement = document.createElement('div');
+        tempDivElement.id = 'player' + String(id);
+        document.getElementById('playerContainer' + String(id)).appendChild(tempDivElement);
+
+        // build YT.Player object wich replaces the tempDivElement with the matching id
+        let videoId = this.videoId(url)
+
+        let player = new YT.Player(tempDivElement.id, {
+
+            videoId: videoId,
+            playerVars: {
+
+                fs: 0,
+                autoplay: 0,
+                showinfo: 2,
+                controls: 0,
+                disablekb: 1,
+                iv_load_policy: 3,
+                allowfullscreen: 0,
+            },
+            events: {
+
+                onReady: onPlayerReady,
+                onStateChange: onPlayerStateChange
+            },
+        });
+        // add the iframe to player class
+        document.getElementById(tempDivElement.id).classList.add('player');
+
+        // class variables
         this.url = url;
-        this.domId = domId;
-        this.vidId = this.videoId(url);
+        this.id = id;
+        this.vidId = videoId;
         this.selected = false;
+        this.playing = false;
         this.volumeTarget = 100;
         this.describtion = "no describtion";
+        this.container = playerContainer;
+        this.picker = picker;
+        this.player = player;
+        //...
+
+        // add volumeBar
+        let infoBar = document.createElement('div');
+        infoBar.id = "volumeBar" + String(id);
+        infoBar.classList.add('infoBar');
+        infoBar.style.opacity = 0.0;
+        infoBar.innerHTML = "| playing | volume 100 |";
+        playerContainer.appendChild(infoBar);
+
     }
 
     // convert video-url to videoId
@@ -54,15 +129,17 @@ class Tune {
         return (match && match[7].length == 11) ? match[7] : false;
     }
 
-
+    // for viz distinction (contrast)
+    randColor() {
+        let r = Math.floor(Math.random() * 55);
+        let g = Math.floor(Math.random() * 50);
+        let b = Math.floor(Math.random() * 50);
+        return "rgb(" + r + "," + g + "," + b + ")";
+    }
 }
 
-tina = new Tune("https://www.youtube.com/watch?v=aW9wBjkpWIE", "peter");
-console.log(tina.info);
-
-
-// Default urls
-let urls = [
+// default urls
+let defaultUrls = [
     "https://www.youtube.com/watch?v=Jikm8CCRbdM",
     "https://www.youtube.com/watch?v=JyyQlYRqvRs",
     "https://www.youtube.com/watch?v=HAw37tUHcOo",
@@ -101,12 +178,12 @@ let urls = [
     "https://www.youtube.com/watch?v=-MwZxXwsSOs"
 ]
 
-let numPlayers = urls.length;
-numPlayers = 21;
+// contains tune objects
+let tunes = [];
 
+// load yt-player-api
 loadScript();
 
-// Load the YT Player-API
 function loadScript() {
 
     if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
@@ -117,100 +194,32 @@ function loadScript() {
     }
 }
 
-//Convert video-url to videoID
-function getYouTubeVideoId(url) {
-
-    let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/; // ???
-    let match = url.match(regExp);
-    return (match && match[7].length == 11) ? match[7] : false;
-}
-
-function clearSelection() {
-
-    let pickers = document.getElementsByClassName('picker');
-    for (var i = 0; i < pickers.length; i++) {
-        pickers[i].style.opacity = 0.7;
-    }
-}
-
-// Add player, container, picker     
-function addPlayer(url, index) {
-
-    // Create a Container for player positioning
-    let playerContainer = document.createElement('div');
-    playerContainer.id = 'playerContainer' + String(index);
-    playerContainer.classList.add('playerContainer');
-    document.getElementById('playerContainerSection').appendChild(playerContainer);
-
-    // Create cover divs to tint if selected and to fade in 
-    let picker = document.createElement('div');
-    picker.id = 'picker' + index;
-    picker.classList.add('picker');
-    document.getElementById('playerContainer' + String(index)).appendChild(picker);
-
-    // Add onclkick functions to pickers
-    picker.onclick = function(event) {
-
-        picker.style.opacity = 0.0;
-        //document.getElementById('infoText').innerHTML = picker.id;
-    }
-
-    // Create a temp div to be replaced by the video-iframe
-    let tempDivElement = document.createElement('div');
-    tempDivElement.id = 'player' + String(index);
-    document.getElementById('playerContainer' + String(index)).appendChild(tempDivElement);
-
-    // Create a YT.Player object wich replaces the tempDivElement with the matching id
-    let player = new YT.Player(tempDivElement.id, {
-
-        videoId: getYouTubeVideoId(url),
-        playerVars: {
-
-            fs: 0,
-            autoplay: 0,
-            showinfo: 2,
-            controls: 0,
-            disablekb: 1,
-            iv_load_policy: 3,
-            allowfullscreen: 0,
-        },
-        events: {
-
-            onReady: onPlayerReady,
-            onStateChange: onPlayerStateChange
-        },
-    });
-    // add the iframe to the player class
-    document.getElementById('player' + index).classList.add('player');
-}
-// This function is called by YouTube once the the API is ready
+// called once the api is ready
 function onYouTubeIframeAPIReady() {
-    // add the first player
-    addPlayer(urls[0], 0);
+    tuneCounter = 0;
+    tunes.push(new Tune(defaultUrls[tuneCounter], tuneCounter));
 }
-// to count ready players (to add after each other)
-let readyCounter = 0;
-// Is called when a Player is ready
+
+// called when a player is ready
 function onPlayerReady(event) {
 
-    // fading effect for each player
-    let picker = document.getElementById('picker' + String(readyCounter));
-    setTimeout(function() {
+    //console.log(tunes[0].player.getPlayerState());
 
-        picker.style.opacity = 0.0;
-        picker.style.transition = 'opacity 0.7s';
-        picker.style.opacity = 0.7;
-        picker.style.pointerEvents = 'auto';
+    // to hide channel-logo and video-title
+    event.target.playVideo();
+    event.target.stopVideo();
 
-    }, 1000);
+    // to reveal the videoplayer viz
+    tunes[tuneCounter].picker.style.opacity = 0.3;
 
-    // add next player when prev ready
-    readyCounter += 1;
-    if (readyCounter < numPlayers) {
+    tuneCounter += 1;
+    let num = defaultUrls.length;
+    num = 16; //
+    if (tuneCounter < num) {
 
-        addPlayer(urls[readyCounter], readyCounter);
+        // add new tune object
+        tunes.push(new Tune(defaultUrls[tuneCounter], tuneCounter));
     }
-
 }
 
 // Is called when a player changes state
