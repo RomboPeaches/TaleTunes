78a44032746669fdeb4c2e975d55c02b90124da1
Robin <robinbenjaminvanek@gmail.com>
2023-02-18 07:19:40 +0100

    added volume increase steps and removed drop after button push after step volume commands

--- app.js
+++ app.js
@@ -1,17 +1,63 @@
 // TODO
 
-// indicate running, volume, what else?
-// shortcuts?!
-// drag and drop url defaultUrls !!!
-// save via cookie or file (defaultUrls, set size, discription?
-// OR SELECT EFFECT i.e. FADE IN FAST and then click the iframe proxy elem
-// and vice versa: selct multiple iframes and choose effect BOTH
-// fade speed setting
-// label opt. hide labels 
+// urls string id part to color to genrate always same color code for tunes!?,  
+
+// drag and drop url saved urls and settings?! !!! json files !!!
+
+// forbid selection until a player ready
+
+// save via json file
+
+// slowed or stopped setInterval function fix?
+
+// add tunes (urls)
+
 // tooltip for describtion and way to add descriptions
 
-// fade more slow for target 100 and 0 !!!
+// are the tune elem's ids i.e. picker, container, player obsolete?
 
+// visualize volume
+
+
+let defaultUrls = [
+
+    "https://www.youtube.com/watch?v=Jikm8CCRbdM",
+    "https://www.youtube.com/watch?v=JyyQlYRqvRs",
+    "https://www.youtube.com/watch?v=HAw37tUHcOo",
+    "https://www.youtube.com/watch?v=ccj1AiFE02U",
+    "https://www.youtube.com/watch?v=synJbsrk0k8",
+    "https://www.youtube.com/watch?v=HsdlX1E-Tv8",
+    "https://www.youtube.com/watch?v=So0H1kyn6FY",
+    "https://www.youtube.com/watch?v=ddMSMwKQkKI",
+    "https://www.youtube.com/watch?v=4E-_Xpj0Mgo",
+    "https://www.youtube.com/watch?v=hCVA_0vRisw",
+    "https://www.youtube.com/watch?v=5Jzp5H4mQVE",
+    "https://www.youtube.com/watch?v=Lj6Iv7plogM",
+    "https://www.youtube.com/watch?v=ZnCMLyN4gQ4",
+    "https://www.youtube.com/watch?v=RPkHu8M_U4c",
+    "https://www.youtube.com/watch?v=yVMckBB66-w",
+    "https://www.youtube.com/watch?v=665La2ZY7pA",
+    "https://www.youtube.com/watch?v=U-iHnbPb60Y",
+    "https://www.youtube.com/watch?v=0yTKiFXTdeI",
+    "https://www.youtube.com/watch?v=OT2IfhFYVZw",
+    "https://www.youtube.com/watch?v=aW9wBjkpWIE",
+    "https://www.youtube.com/watch?v=FxOtJq-Tkys",
+    "https://www.youtube.com/watch?v=grFQygwA27k",
+    "https://www.youtube.com/watch?v=EcLZE4KVc_E",
+    "https://www.youtube.com/watch?v=QlXrndlCFIk",
+    "https://www.youtube.com/watch?v=n--JsJ3AlKA",
+    "https://www.youtube.com/watch?v=WW4bdFhcZgw",
+    "https://www.youtube.com/watch?v=7wBzL62Va1k",
+    "https://www.youtube.com/watch?v=rSkX8kQ3wh4",
+    "https://www.youtube.com/watch?v=t3B802PIuB0",
+    "https://www.youtube.com/watch?v=P-EVYJ6GSpI",
+    "https://www.youtube.com/watch?v=L_gH2MdVL78",
+    "https://www.youtube.com/watch?v=KvXRg6HlDoU",
+    "https://www.youtube.com/watch?v=tpi5qsXm_cM",
+    "https://www.youtube.com/watch?v=zZxiJkwPJ10",
+    "https://www.youtube.com/watch?v=-MwZxXwsSOs",
+    "https://www.youtube.com/watch?v=-MwZxXwsSOs"
+]
 
 // to load yt iframe api
 if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
@@ -26,11 +72,15 @@ if (typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') {
 function onYouTubeIframeAPIReady() {
 
     tuneGroup = new TuneGroup();
-    tuneGroup.addUrls(defaultUrls.slice(0, 5));
+    //tuneGroup.addUrls(defaultUrls);
+    tuneGroup.addUrls(defaultUrls.slice(0, 3));
 }
 
 // called when a player is ready
-function onPlayerReady(event) { tuneGroup.updateTunes(); }
+function onPlayerReady(event) {
+
+    tuneGroup.updateTunes();
+}
 
 // called when a player changes state
 function onPlayerStateChange(event) {}
@@ -44,20 +94,19 @@ class TuneGroup {
         this.urls = [];
         this.tunes = [];
 
-        this.worker = new Worker(setInterval(function() {
+        setInterval(function() {
             tuneGroup.updateVolumes();
-        }, 100));
+        }, 100);
     }
 
-    // stages new urls
+    // adds new urls to create tunes from
     addUrls(newUrls) {
 
         this.urls = this.urls.concat(newUrls);
-        // called updateTunes once 
         this.updateTunes();
     }
 
-    // called by addUrls() or when a player is ready
+    // called once by addUrls and when a player is ready
     updateTunes() {
 
         // make one tune for a url that has no corresponding tune 
@@ -86,15 +135,41 @@ class TuneGroup {
     updateVolumes() {
 
         this.tunes.forEach(tune => {
-            let currentVolume = tune.player.getVolume();
-            if (currentVolume != tune.faidTarget) {
-                if (currentVolume < tune.faidTarget) {
-                    tune.player.setVolume(currentVolume + 1);
-                } else {
-                    tune.player.setVolume(currentVolume - 1)
+
+            try {
+
+                let volumeNow = tune.player.getVolume();
+                // if not done fading
+                if (volumeNow != tune.faidTarget) {
+
+                    tune.stepSize = 1;
+
+                    let close = false; // when close to target
+                    if (Math.abs(volumeNow - tune.faidTarget) < 10) {
+
+                        // slow fading 
+                        if (tune.delayCounter > 4) {
+                            tune.stepSize = 1;
+                            tune.delayCounter = 0;
+                        } else {
+                            tune.delayCounter += 1;
+                            tune.stepSize = 0;
+                        }
+                    }
+
+                    // increase or ...
+                    if (volumeNow < tune.faidTarget) {
+                        tune.player.setVolume(volumeNow + tune.stepSize);
+                    } else {
+                        // ... decrease Volume
+                        tune.player.setVolume(volumeNow - tune.stepSize)
+                    }
+                    console.log("vol:", volumeNow);
+
                 }
-                console.log("active fading:", currentVolume);
-            }
+
+
+            } catch { /* in case a player has not been ready */ }
         });
     }
 
@@ -110,7 +185,7 @@ class TuneGroup {
         });
     }
 
-    // play selected tunes
+    // pause selected tunes
     pauseSelected() {
 
         this.tunes.forEach(tune => {
@@ -168,6 +243,12 @@ class TuneGroup {
 
         this.tunes.forEach(tune => {
             if (tune.selected == true) {
+
+                // if not playing
+                if (tune.player.getPlayerState() != 1) {
+                    tune.player.setVolume(0);
+                    tune.player.playVideo();
+                }
                 tune.selected = false;
                 tune.picker.style.opacity = 0.2;
                 tune.player.playVideo();
@@ -187,6 +268,63 @@ class TuneGroup {
             }
         });
     }
+
+    // increase volume step
+    volumeStepIncrease() {
+
+        this.tunes.forEach(tune => {
+            if (tune.selected == true) {
+
+                let newTarget = tune.player.getVolume() + 25;
+                if (newTarget > 100) { newTarget = 100; }
+
+                tune.faidTarget = newTarget;
+                console.log("newTarget: ", newTarget);
+            }
+        });
+    }
+
+    // decrease volume step
+    volumeStepDecrease() {
+
+        this.tunes.forEach(tune => {
+            if (tune.selected == true) {
+
+                let newTarget = tune.player.getVolume() - 25;
+                if (newTarget < 0) { newTarget = 0; }
+
+                tune.faidTarget = newTarget;
+                console.log("newTarget: ", newTarget);
+            }
+        });
+    }
+
+    // mute by setting volume to 0 immediately
+    muteSelected() {
+
+        this.tunes.forEach(tune => {
+            if (tune.selected == true) {
+                tune.selected = false;
+                tune.picker.style.opacity = 0.2;
+                tune.faidTarget = 0;
+                tune.player.setVolume(0);
+            }
+        });
+    }
+
+    // unmute by setting volume to 100 immediately
+    unMuteSelected() {
+
+        this.tunes.forEach(tune => {
+            if (tune.selected == true) {
+                tune.selected = false;
+                tune.picker.style.opacity = 0.2;
+                tune.faidTarget = 0;
+                tune.player.setVolume(100);
+            }
+        });
+    }
+
 }
 
 // object to reference playerContainer-, player-, picker-elements, 
@@ -250,7 +388,9 @@ class Tune {
         this.playing = false;
         this.selected = false;
         this.faidTarget = 100;
-        this.playerReady = false;
+        this.delayCounter = 0;
+        this.stepSize = 1;
+        //this.playerReady = false;
         this.describtion = "no describtion";
 
         this.player = player;
@@ -274,42 +414,3 @@ class Tune {
         return "rgb(" + r + "," + g + "," + b + ")";
     }
 }
-// default urls
-let defaultUrls = [
-    "https://www.youtube.com/watch?v=Jikm8CCRbdM",
-    "https://www.youtube.com/watch?v=JyyQlYRqvRs",
-    "https://www.youtube.com/watch?v=HAw37tUHcOo",
-    "https://www.youtube.com/watch?v=ccj1AiFE02U",
-    "https://www.youtube.com/watch?v=synJbsrk0k8",
-    "https://www.youtube.com/watch?v=HsdlX1E-Tv8",
-    "https://www.youtube.com/watch?v=So0H1kyn6FY",
-    "https://www.youtube.com/watch?v=ddMSMwKQkKI",
-    "https://www.youtube.com/watch?v=4E-_Xpj0Mgo",
-    "https://www.youtube.com/watch?v=hCVA_0vRisw",
-    "https://www.youtube.com/watch?v=5Jzp5H4mQVE",
-    "https://www.youtube.com/watch?v=Lj6Iv7plogM",
-    "https://www.youtube.com/watch?v=ZnCMLyN4gQ4",
-    "https://www.youtube.com/watch?v=RPkHu8M_U4c",
-    "https://www.youtube.com/watch?v=yVMckBB66-w",
-    "https://www.youtube.com/watch?v=665La2ZY7pA",
-    "https://www.youtube.com/watch?v=U-iHnbPb60Y",
-    "https://www.youtube.com/watch?v=0yTKiFXTdeI",
-    "https://www.youtube.com/watch?v=OT2IfhFYVZw",
-    "https://www.youtube.com/watch?v=aW9wBjkpWIE",
-    "https://www.youtube.com/watch?v=FxOtJq-Tkys",
-    "https://www.youtube.com/watch?v=grFQygwA27k",
-    "https://www.youtube.com/watch?v=EcLZE4KVc_E",
-    "https://www.youtube.com/watch?v=QlXrndlCFIk",
-    "https://www.youtube.com/watch?v=n--JsJ3AlKA",
-    "https://www.youtube.com/watch?v=WW4bdFhcZgw",
-    "https://www.youtube.com/watch?v=7wBzL62Va1k",
-    "https://www.youtube.com/watch?v=rSkX8kQ3wh4",
-    "https://www.youtube.com/watch?v=t3B802PIuB0",
-    "https://www.youtube.com/watch?v=P-EVYJ6GSpI",
-    "https://www.youtube.com/watch?v=L_gH2MdVL78",
-    "https://www.youtube.com/watch?v=KvXRg6HlDoU",
-    "https://www.youtube.com/watch?v=tpi5qsXm_cM",
-    "https://www.youtube.com/watch?v=zZxiJkwPJ10",
-    "https://www.youtube.com/watch?v=-MwZxXwsSOs",
-    "https://www.youtube.com/watch?v=-MwZxXwsSOs"
-]
